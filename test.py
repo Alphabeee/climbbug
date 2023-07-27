0c8205245a0bca91a692e71f5fd08076bf88a8d9
Alphabeee <bob20031002@gmail.com>
2023-07-27 14:26:53 +0800

    hide

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-for i in range(0,100):
-    with open('screenshot.png', 'rb') as f:
-    img = f.read()
+import ddddocr
+from PIL import Image
+ocr = ddddocr.DdddOcr()
+for i in range(0, 100):
+    with open(f'picture/{i}.png', 'rb') as f:
+        img = f.read()
+    text = ocr.classification(img)
+    text += '\n'
+    with open('test.txt', 'a', encoding='UTF-8') as f:
+        f.write(text)
